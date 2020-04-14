568d0a143810d2ca5216253fbce60beeae3a18b1
Alex Miller <alexmiller@gmail.com>
2020-04-13 21:18:37 -0700

    initial multiplayer setup with mirror

---
+++ Assets/Scripts/RandomizePosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

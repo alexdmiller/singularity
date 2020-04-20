aa507cfc7259a976d4f4b7e6b660dd92f2c5474a
Maxwell Forbes <m.b.forbes@gmail.com>
2020-04-19 20:11:41 -0700

    Movement WASSUPPPPPP

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var networkIdentity = GetComponent<Mirror.NetworkIdentity>();
+        if (!networkIdentity.hasAuthority)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+}
